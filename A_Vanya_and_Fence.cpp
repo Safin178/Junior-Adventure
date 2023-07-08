349bd679547f853ef40398993379bb06fdf75847
Afrin Hasan Safin <redwanulhoque37@gmail.com>
2023-07-08 12:19:15 +0600

    AC

---
+++ A_Vanya_and_Fence.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+void solve()
+{
+    
+    int n, x;
+    cin >> n >> x;
+    int b[n], sum = 0;
+
+    for(int i = 0 ;  i  <n; i++)
+    {
+        cin >> b[i];
+        if(b[i] > x) sum += 2;
+        else sum += 1;
+    }
+
+    cout << sum;
+
+
+
+
+
+
+}
+int main(){
+    int t=1;
+    //cin >> t;          // removr '//' for testcase
+    while(t--){
+        solve();
+    }
+    return 0;
+}
