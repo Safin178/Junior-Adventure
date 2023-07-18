862d599d1c7da2c722c1c7e203cb84f3ce0cd21d
Afrin Hasan Safin <redwanulhoque37@gmail.com>
2023-07-18 23:44:16 +0600

    18 /07

---
+++ 800 problem set/B_Triple.cpp
@@ -0,0 +1,64 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef vector<long long> vl;
+typedef vector<int> vi;
+
+#define all(v)	((v).begin()), ((v).end())
+#define sz(v)	((int)((v).size()))
+#define ll long long
+#define pb push_back
+#define nl '\n'
+#define yes cout<<"YES\n"
+#define no cout<<"NO\n"
+#define yesR cout<<"YES\n";return;
+#define noR cout<<"NO\n";return;
+#define vit vector<int>::iterator  
+#define forcin(n) for(auto &x : n) cin>>x;
+#define forcout(n) for(auto x : n) cout << x << " ";
+void printVec(vector<int> v2, string s = "")//vector ke print kore just printVec(v) likle hobe
+{
+	cout<<s;
+	for (int i = 0; i < (int)v2.size(); ++i)
+			cout<<v2[i]<<" ";
+		cout<<"\n";
+}
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vi f(n+1);
+    int x;
+
+    for(int i = 0; i < n; i++)
+    {
+        cin >> x;
+        f[x]++;
+
+    }
+
+    for(int i = 1 ; i <= n ; i++)
+    {
+        if(f[i] >= 3)
+        {
+            cout << i << nl;
+            return;
+        }
+    }
+    
+    cout << -1 << nl;
+
+
+
+
+}
+
+int main(){
+    int t=1;
+    cin >> t;          // remove '//' for testcase
+    while(t--){
+        solve();
+    }
+    return 0;
+}
