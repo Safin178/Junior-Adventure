025021dbd839e07d18e3e2b296f3352916caf69e
Afrin Hasan Safin <redwanulhoque37@gmail.com>
2023-08-12 12:34:15 +0600

    08/12/2023

---
+++ 900/A_Odd_Divisor.cpp
@@ -0,0 +1,93 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef vector<long long> vl;
+typedef vector<int> vi;
+
+#define all(v)	((v).begin()), ((v).end())
+#define sz(v)	((int)((v).size()))
+#define ll long long
+#define pb push_back
+#define nl '\n'
+#define yes cout<<"YES\n"
+#define no cout<<"NO\n"
+#define yesR cout<<"YES\n";return;
+#define noR cout<<"NO\n";return;
+#define vit vector<int>::iterator  
+#define forcin(n) for(auto &x : n) cin>>x;
+#define forcout(n) for(auto x : n) cout << x << " ";
+void printVec(vector<int> v2, string s = "")//vector ke print kore just printVec(v) likle hobe
+{
+	cout<<s;
+	for (int i = 0; i < (int)v2.size(); ++i)
+			cout<<v2[i]<<" ";
+		cout<<"\n";
+}
+/*        ／＞　　フ
+　　　　　| 　_　 _ |
+　 　　　／` ミ＿Yノ     meow
+　　 　 /　　　 　 |   
+　　　 /　 ヽ　　 ﾉ  
+　 　 │　　|　|　|
+　／￣|　　 |　|　|    
+　| (￣ヽ＿_ヽ_)__)   
+　＼二つ
+*/
+
+void solve()
+{
+    ll n;
+    cin >> n;
+
+    // for(ll i = 3 ;i <= n; i+=2)
+    // {
+    //     if(n % i == 0)
+    //     {
+    //         yesR;
+    //     }
+    // }
+    //no;  
+
+    // ll x = 0;
+    // if(n % 2 ==0)
+    // {
+    //      x = n/2;
+    // }
+    // if( n % 2 == 0 && x % 2 == 0 || x == 1)
+    // {
+    //     no;
+    // }   
+    // else yes;
+    if(n % 2 != 0)
+    {
+        //cout << "here: ";
+        yesR;
+        
+    }
+    else{
+        for(ll i = n/2; i > 1; i/= 2)
+        {
+            if(i % 2 != 0)
+            {
+                yesR;
+            }
+        }
+    }
+    
+    no;
+   
+
+    
+
+
+
+}
+
+int main(){
+    int t=1;
+    cin >> t;          // remove '//' for testcase
+    while(t--){
+        solve();
+    }
+    return 0;
+}
---
+++ 900/B_Sale.cpp
@@ -0,0 +1,74 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef vector<long long> vl;
+typedef vector<int> vi;
+
+#define all(v)	((v).begin()), ((v).end())
+#define sz(v)	((int)((v).size()))
+#define ll long long
+#define pb push_back
+#define nl '\n'
+#define yes cout<<"YES\n"
+#define no cout<<"NO\n"
+#define yesR cout<<"YES\n";return;
+#define noR cout<<"NO\n";return;
+#define vit vector<int>::iterator  
+#define forcin(n) for(auto &x : n) cin>>x;
+#define forcout(n) for(auto x : n) cout << x << " ";
+void printVec(vector<int> v2, string s = "")//vector ke print kore just printVec(v) likle hobe
+{
+	cout<<s;
+	for (int i = 0; i < (int)v2.size(); ++i)
+			cout<<v2[i]<<" ";
+		cout<<"\n";
+}
+/*        ／＞　　フ
+　　　　　| 　_　 _ |
+　 　　　／` ミ＿Yノ     meow
+　　 　 /　　　 　 |   
+　　　 /　 ヽ　　 ﾉ  
+　 　 │　　|　|　|
+　／￣|　　 |　|　|    
+　| (￣ヽ＿_ヽ_)__)   
+　＼二つ
+*/
+
+void solve()
+{
+   int n, m;
+   cin >> n >> m;
+   vi v(n), f;
+   forcin(v);
+
+   for (int i = 0; i < sz(v); i++)
+   {
+    if(v[i] <= 0)
+    {
+        f.pb(v[i]);
+    }
+
+   }
+   int cnt = 0;
+   sort(all(f));
+   //reverse(all(f));
+   for(int i = 0 ;i < min(sz(f), m); i++)
+   {
+        cnt += (-1) * f[i];
+   }
+   cout << cnt << nl;
+   
+    
+
+
+
+}
+
+int main(){
+    int t=1;
+    //cin >> t;          // remove '//' for testcase
+    while(t--){
+        solve();
+    }
+    return 0;
+}
