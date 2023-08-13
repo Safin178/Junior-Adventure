6181409700350c8353d56526bf2c33da28d45db3
Afrin Hasan Safin <redwanulhoque37@gmail.com>
2023-08-13 21:28:00 +0600

    WA F

---
+++ 900/F_Football.cpp
@@ -0,0 +1,69 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef vector<long long> vl;
+typedef vector<int> vi;
+
+#define all(v)	((v).begin()), ((v).end())
+#define sz(v)	((int)((v).size()))
+#define ll long long
+#define pb push_back
+#define nl '\n'
+#define yes cout<<"YES\n"
+#define no cout<<"NO\n"
+#define yesR cout<<"YES\n";return;
+#define noR cout<<"NO\n";return;
+#define vit vector<int>::iterator  
+#define forcin(n) for(auto &x : n) cin>>x;
+#define forcout(n) for(auto x : n) cout << x << " ";
+void printVec(vector<int> v2, string s = "")//vector ke print kore just printVec(v) likle hobe
+{
+	cout<<s;
+	for (int i = 0; i < (int)v2.size(); ++i)
+			cout<<v2[i]<<" ";
+		cout<<"\n";
+}
+/*        ／＞　　フ
+　　　　　| 　_　 _ |
+　 　　　／` ミ＿Yノ     meow is sad
+　　 　 /　　　 　 |   
+　　　 /　 ヽ　　 ﾉ  
+　 　 │　　|　|　|
+　／￣|　　 |　|　|    
+　| (￣ヽ＿_ヽ_)__)   
+　＼二つ
+*/
+
+void solve()
+{
+   int n;
+   int x,y;
+   cin >> n >> x >> y;
+
+    x= min(x,y);
+    y = max(x,y);
+
+    for(int i = 0; i < n; i++,x = 0, y--)
+    {
+        cout <<x <<':' << k <<nl;
+        if(y<= 0)
+        {
+            y = 1;
+        }
+
+
+    }//give up a work to do,,
+    
+
+
+
+}
+
+int main(){
+    int t=1;
+    //cin >> t;          // remove '//' for testcase
+    while(t--){
+        solve();
+    }
+    return 0;
+}
